91028e966146495d57bb86038b67f9457421c374
Sorin Turda <s.turda12@gmail.com>
2023-11-15 17:50:22 +0200

    lab 06 termina tema 3.2 cu rechizite

---
+++ 06/rechizita/src/Manual.java
@@ -0,0 +1,5 @@
+public class Manual extends Rechizita{
+    public String getNume(){
+        return this.eticheta;
+    }
+}
---
+++ 06/rechizita/src/Rechizita.java
@@ -0,0 +1,6 @@
+public abstract class Rechizita {
+    protected String eticheta;
+
+    protected abstract String getNume();
+
+}
