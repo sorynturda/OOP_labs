9c259e6c873e2c4251a1e1996895e048c6627c55
Sorin Turda <s.turda12@gmail.com>
2023-12-07 13:00:32 +0200

    nou buton de reset

--- curs_swing/src/Main.java
+++ curs_swing/src/Main.java
@@ -9,6 +9,7 @@ public class Main {
     JPanel jpane = new JPanel();
     JLabel jlabel = new JLabel("contor");
     JButton btn1 = new JButton("0");
+    JButton btn2 = new JButton("ReS3t");
 
     public static void main(String[] args) {
         Main cls = new Main();
@@ -18,19 +19,30 @@ public class Main {
     public void createGUI() {
         btn1.setFont(new Font("Arial", Font.BOLD, 72));
         btn1.addActionListener(new ButtonListener());
+        btn2.setFont(new Font("Arial", Font.BOLD, 72));
+        btn2.addActionListener(new Reset());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 300);
         jpane.add(jlabel);
-        jpane.add(jlabel);
-        frame.add(btn1);
+        jpane.add(btn1);
+        jpane.add(btn2);
+        frame.add(jpane);
         frame.setVisible(true);
     }
 
     public class ButtonListener implements ActionListener {
+        @Override
         public void actionPerformed(ActionEvent e) {
             contor++;
             btn1.setText(contor + "");
         }
     }
 
+    public class Reset implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            contor = 0;
+            btn1.setText("0");
+        }
+    }
 }
